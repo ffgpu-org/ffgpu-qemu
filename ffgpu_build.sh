e324cee970236250b55fd22f2aca9141e762059e
shaowei <shaowei@ffgpu.org>
2020-01-17 17:34:42 +0800

    Add build script for FFGPU

---
+++ ffgpu_build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+ffgpu_conf()
+{
+    OPTS=" --enable-gtk --enable-vte --enable-virtfs --enable-kvm "
+    OPTS+=" --disable-user --disable-sdl --target-list=x86_64-softmmu "
+    echo "configure with $OPTS"
+    ./configure $OPTS
+}
+
+ffgpu_make()
+{
+    NCPU=`getconf _NPROCESSORS_CONF`
+    make -j ${NCPU}
+}
+
+if [ $# -eq 0 ];then
+    ffgpu_conf && ffgpu_make
+else
+    while getopts "cm" opts;do
+	case "$opts" in
+	    "c")
+		ffgpu_conf
+		;;
+	    "m")
+		ffgpu_make
+		;;
+	esac
+    done
+fi
